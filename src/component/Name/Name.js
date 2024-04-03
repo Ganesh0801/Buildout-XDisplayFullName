262e2d94709ea07a3336791c2f5209f833bdbd62
Ganesh <ganeshsbsa08@gmail.com>
2024-04-03 13:51:47 +0530

    first commit

---
+++ src/component/Name/Name.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styles from "./Name.module.css";
+
+const Name = () => {
+    const [send,setSend] = useState(false)
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName] = useState("")
+
+const handleSubmit = (e)=>{
+    e.preventDefault();
+
+    if(firstName !== "" && lastName !== ""){
+        setSend(true)
+    }
+    
+}
+
+const handleChange = (e)=>{
+  
+  if(e.target.id === "first"){
+    setFirstName(e.target.value)
+  }
+  else{
+    setLastName(e.target.value)
+  }
+}
+  
+return (
+   <>
+    <form>
+      <h1>Full Name Display</h1>
+      <div className={styles.container}>
+            <div>
+                    <lable htmlFor="first">First Name : </lable>
+                    <input 
+                    id="first"
+                    type='text' 
+                    value={firstName}
+                    onChange={handleChange}/>
+            </div>
+            <div>
+                    <lable htmlFor="last">Last Name : </lable>
+                    <input 
+                    id="last"
+                    type='text'
+                    value={lastName}
+                    onChange={handleChange}/>
+            </div>
+            <button type='submit' onClick={handleSubmit}>Submit</button>
+     </div>
+    </form>
+    {send && (
+        <p>Full Name : {firstName} {lastName}</p>
+    )}
+   </>
+  )
+}
+
+export default Name
